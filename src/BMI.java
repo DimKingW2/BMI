7a4251bf03f91d613fd0ba067849f35861b22194
Dimitri King <Dimitri.king.2022@student.lhschools.org>
2018-09-06 08:42:32 -0400

    changed variables

--- src/BMI.java
+++ src/BMI.java
@@ -3,7 +3,7 @@ public class BMI {
     public static void main(String[] args){
        // Defining stuff
         float inch;
-        float m;
+        float meters;
         float pounds;
         float kg;
         float bmi;
@@ -14,13 +14,13 @@ public class BMI {
         keyboard = new Scanner(System.in);
         System.out.println("What is your height in inches?");
         inch = keyboard.nextFloat();
-        m = (float) (inch * (0.0254));
+        meters = (float) (inch * (0.0254));
         System.out.println("Great, now what is your weight in pounds.");
         pounds = keyboard.nextFloat();
         kg = (float) (pounds / (2.20462/1));
 
         // calculations
-        bmi = (kg/(m*m));
+        bmi = (kg/(meters*meters));
 
         //gives you bmi
         System.out.println("Your bmi is, " + bmi + ".");
