6936785c1ec9a62fba58b9b1d1a4ac050af9a4df
Dimitri King <Dimitri.king.2022@student.lhschools.org>
2018-09-06 08:34:28 -0400

    added a check to see if your overweight.

--- src/BMI.java
+++ src/BMI.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class BMI {
     public static void main(String[] args){
+       // Defining stuff
         float inch;
         float m;
         float pounds;
@@ -9,7 +10,7 @@ public class BMI {
         Scanner keyboard;
 
 
-
+        // Getting info on people
         keyboard = new Scanner(System.in);
         System.out.println("What is your height in inches?");
         inch = keyboard.nextFloat();
@@ -18,10 +19,22 @@ public class BMI {
         pounds = keyboard.nextFloat();
         kg = (float) (pounds / (2.20462/1));
 
-
+        // calculations
         bmi = (kg/(m*m));
 
+        //gives you bmi
         System.out.println("Your bmi is, " + bmi + ".");
+        //tells you if you are overweight, average, or underweight
+        if (bmi > 25) {
+            System.out.println("You are overweight.");
+        }
+       else if (bmi < 18.5) {
+            System.out.println("You are underweight.");
+        }
+        else {
+            System.out.println("You are average.");
+        }
+
 
 
 
